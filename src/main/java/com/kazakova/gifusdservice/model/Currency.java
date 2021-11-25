41ee448b810a17346231cc2e66caacac7c01019d
VCKazakova <v.c.kazakova@gmail.com>
2021-11-25 13:43:44 +0300

    master create model and controller

--- src/main/java/com/kazakova/gifusdservice/model/Currency.java
+++ src/main/java/com/kazakova/gifusdservice/model/Currency.java
@@ -1,7 +1,21 @@
 package com.kazakova.gifusdservice.model;
 
-public enum Currency {
-    EUR,
-    GBP,
-    RUB;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+import java.util.Map;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Currency {
+
+    private String disclaimer;
+    private String license;
+    private LocalDateTime timestamp;
+    private String base;
+    private Map<String, BigDecimal> rates;
 }
