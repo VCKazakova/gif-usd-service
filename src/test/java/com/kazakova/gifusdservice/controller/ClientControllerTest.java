00ed28f16d86192f35e4ddcf688418a1e4af4f23
VCKazakova <v.c.kazakova@gmail.com>
2021-11-29 13:33:52 +0300

    master create tests for ClientController

---
+++ src/test/java/com/kazakova/gifusdservice/controller/ClientControllerTest.java
@@ -0,0 +1,36 @@
+package com.kazakova.gifusdservice.controller;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockHttpServletRequestBuilder;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+@DisplayName("Client controller должен")
+public class ClientControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+
+    @Test
+    @DisplayName("Делать редирект на страницу с подходящей gif")
+    public void testRedirect() throws Exception {
+
+        String symbols = "RUB";
+
+        MockHttpServletRequestBuilder request = MockMvcRequestBuilders.get("/compare_currency/{symbols}", symbols);
+
+        mockMvc.perform(request)
+                .andExpect(status().is3xxRedirection());
+    }
+
+
+}
