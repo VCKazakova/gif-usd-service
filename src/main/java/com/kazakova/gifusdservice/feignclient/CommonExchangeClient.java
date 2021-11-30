bf2a4359d9f29694c3e8c15c8e4280999ab8b574
VCKazakova <v.c.kazakova@gmail.com>
2021-11-30 08:32:57 +0300

    master javadoc

--- src/main/java/com/kazakova/gifusdservice/feignclient/CommonExchangeClient.java
+++ src/main/java/com/kazakova/gifusdservice/feignclient/CommonExchangeClient.java
@@ -6,6 +6,12 @@ import org.springframework.web.bind.annotation.PathVariable;
 
 public interface CommonExchangeClient {
 
+    /**
+     * @param date    - дата, по которой возвращается курс
+     * @param symbols - валюта, относительно которой считается курс USD
+     * @return Currency - JSON с необходимыми данными
+     */
+
     @GetMapping("/{date}.json?app_id=cfe27e3b63004449900c92e3de48b3eb&symbols={symbols}")
     Currency getCurrencyByDate(@PathVariable("date") String date, @PathVariable("symbols") String symbols);
 
