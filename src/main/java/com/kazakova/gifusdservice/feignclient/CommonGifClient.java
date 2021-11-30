839975d90ad9e5c39bc421a5822d990fb59bfb6b
VCKazakova <v.c.kazakova@gmail.com>
2021-11-30 08:34:54 +0300

    master javadoc

--- src/main/java/com/kazakova/gifusdservice/feignclient/CommonGifClient.java
+++ src/main/java/com/kazakova/gifusdservice/feignclient/CommonGifClient.java
@@ -5,6 +5,12 @@ import org.springframework.web.bind.annotation.PathVariable;
 
 public interface CommonGifClient {
 
+    /**
+     * @param tag - тэг, по которому возвращается рандомный gif-файл
+     * @return String - url подходящего gif-файла
+     */
+
+
     @GetMapping(value = "/random?api_key=u8PWXRI3VIgnUhHPeCW99zq1kurE9vLf&q={tag}")
     String getGif(@PathVariable("tag") String tag);
 
