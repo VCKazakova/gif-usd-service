00bf0d5368ba1ada1935c67d242d989597691cd1
VCKazakova <v.c.kazakova@gmail.com>
2021-11-30 08:19:33 +0300

    master refactoring and add logg

--- src/main/java/com/kazakova/gifusdservice/service/ExchangeService.java
+++ src/main/java/com/kazakova/gifusdservice/service/ExchangeService.java
@@ -2,24 +2,42 @@ package com.kazakova.gifusdservice.service;
 
 import com.kazakova.gifusdservice.feignclient.CommonExchangeClient;
 import com.kazakova.gifusdservice.model.Currency;
-import org.springframework.beans.factory.annotation.Autowired;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
 import java.time.ZonedDateTime;
 
 @Service
+@Slf4j
+@RequiredArgsConstructor
 public class ExchangeService {
 
-    @Autowired
-    private CommonExchangeClient commonExchangeClient;
+
+    private final CommonExchangeClient commonExchangeClient;
+
+    /**
+     * todayDate - вычисление сегодняшней даты
+     * tomorrowDate - вычисление вчерашней даты
+     */
 
     private final String todayDate = ZonedDateTime.now().toLocalDate().toString();
     private final String tomorrowDate = ZonedDateTime.now().minusDays(1).toLocalDate().toString();
 
+    /**
+     * @param symbols - символ валюты, передающийся в endpoint (строка, example: RUB)
+     * @return coefficient - результат сравнения значений вчерашнего и сегодняшнего курса валют
+     * 1 - сегодняшний > вчерашнего
+     * -1 - сегодняшний < вчерашнего
+     * 0 - сегодняшний = вчерашнему
+     */
 
     public int compareCurrency(String symbols) {
+        log.info(">> ExchangeService compareCurrency todayDate={}, tomorrowDate={}, symbols={}", todayDate, tomorrowDate, symbols);
         Currency today = commonExchangeClient.getCurrencyByDate(todayDate, symbols);
         Currency tomorrow = commonExchangeClient.getCurrencyByDate(tomorrowDate, symbols);
-        return today.getRates().get(symbols).compareTo(tomorrow.getRates().get(symbols));
+        int coefficient = today.getRates().get(symbols).compareTo(tomorrow.getRates().get(symbols));
+        log.info("<< ExchangeService compareCurrency  coefficient={}", coefficient);
+        return coefficient;
     }
 }
