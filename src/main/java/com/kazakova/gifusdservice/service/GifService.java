9f14cbc5e32c6ce26f6d8888245d857585cc892d
VCKazakova <v.c.kazakova@gmail.com>
2021-11-30 08:29:37 +0300

    master refactoring and add logg

--- src/main/java/com/kazakova/gifusdservice/service/GifService.java
+++ src/main/java/com/kazakova/gifusdservice/service/GifService.java
@@ -1,35 +1,49 @@
 package com.kazakova.gifusdservice.service;
 
 import com.kazakova.gifusdservice.feignclient.CommonGifClient;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
 import org.json.simple.parser.ParseException;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.HashMap;
 import java.util.Map;
 
+@Slf4j
 @Service
+@RequiredArgsConstructor
 public class GifService {
 
-    @Autowired
-    private CommonGifClient commonGifClient;
+    private final CommonGifClient commonGifClient;
+
+    /**
+     * mapOfCoefficient - коллекция коэффицентов и тэгов для возвращения рандомных gif-файлов
+     */
 
     private static final Map<Integer, String> mapOfCoefficient = new HashMap<>();
 
     {
         mapOfCoefficient.put(1, "${gif.rich}");
-        mapOfCoefficient.put(-1, "${gif.broke");
+        mapOfCoefficient.put(-1, "${gif.broke}");
         mapOfCoefficient.put(0, "${gif.zero}");
     }
 
+    /**
+     * @param coefficient - результат сравнения значений вчерашнего и сегодняшнего курса валют
+     * @return url - url подходящего gif-файла
+     */
+
     public String getGif(int coefficient) throws ParseException {
+        log.info(">> GifService getGif coefficient={}", coefficient);
         String responseEntity = commonGifClient.getGif(mapOfCoefficient.get(coefficient));
         JSONParser jsonParser = new JSONParser();
         JSONObject jsonObject = (JSONObject) jsonParser.parse(responseEntity);
         JSONObject data = (JSONObject) jsonObject.get("data");
-        return data.get("embed_url").toString();
+        String url = data.get("embed_url").toString();
+        log.info("<< GifService getGif  url={}", url);
+        return url;
     }
 
 }
